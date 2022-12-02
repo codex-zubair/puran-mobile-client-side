3b38bc7aae5260ba6d0ab416efd7f54a8a2d7b68
Rocko <codingwithrock@gmail.com>
2022-12-02 11:52:50 +0600

    getting instant serach value for query

--- src/Components/SearchField/SearchField.js
+++ src/Components/SearchField/SearchField.js
@@ -7,19 +7,27 @@ const SearchField = () => {
     // Searching Field implementing
     const searchFiledHandler = (event)=> {
 
+
         event.preventDefault();
+        // Getting Search Value for query
         const form = event.target;
-        const search_text = form.search_text.value;
-        console.log(search_text);
+        const search_text = form.value
+
+
+        
+       
+
+
+
 
     }
 
 
 
     return (
-        <form onSubmit={searchFiledHandler} className='items-center my-0 bg-primary flex justify-center'>
+        <form onChange={searchFiledHandler} className='items-center my-0 bg-primary flex justify-center'>
             <div className='flex items-center w-10/12 justify-center'>
-                <input name='search_text' type="text" className='input my-2 lg:my-10 input-bordered w-8/12 lg:w-5/12' />
+                <input  name='search_text' type="text" className='input my-2 lg:my-10 input-bordered w-8/12 lg:w-5/12' />
                 <button type='submit' className="btn">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                 </button>
