1e3c101fb56dfeb5c5f28f50586408f3a052a075
Rocko <codingwithrock@gmail.com>
2022-12-02 13:16:12 +0600

    serach system are implemented now

--- src/Components/SearchField/SearchField.js
+++ src/Components/SearchField/SearchField.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 
 
@@ -11,11 +12,14 @@ const SearchField = () => {
         event.preventDefault();
         // Getting Search Value for query
         const form = event.target;
-        const search_text = form.value
+        const search_text = form.search_text.value;
+        console.log(search_text)
 
 
-        
+        // Sending search value for query to server.
+        axios.get(`https://puran-mobile-server-side.vercel.app/search/?search=${search_text}`)
        
+
 
 
 
@@ -25,10 +29,10 @@ const SearchField = () => {
 
 
     return (
-        <form onChange={searchFiledHandler} className='items-center my-0 bg-primary flex justify-center'>
+        <form onSubmit={searchFiledHandler} className='items-center my-0 bg-primary flex justify-center'>
             <div className='flex items-center w-10/12 justify-center'>
                 <input  name='search_text' type="text" className='input my-2 lg:my-10 input-bordered w-8/12 lg:w-5/12' />
-                <button type='submit' className="btn">
+                <button  type='submit' className="btn">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                 </button>
 
